4f1b885d8fbce0970fd82ecf6992b28035c704b9
wlcharlie <wl00606352@gmail.com>
2025-04-05 22:04:28 +0800

    feat: 狀態

--- lib/overlays/lobby_tools.dart
+++ lib/overlays/lobby_tools.dart
@@ -3,6 +3,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:work_on_time_game/config/typography.dart';
 import 'package:work_on_time_game/wot_game.dart';
 
+final tabBarIcons = [
+  'assets/images/tabbar_icon_home.png',
+  'assets/images/tabbar_icon_2.png',
+  'assets/images/tabbar_icon_book.png',
+  'assets/images/tabbar_icon_settings.png',
+  'assets/images/tabbar_icon_menu.png',
+];
+
+final statusIcons = [
+  'assets/images/status/mind.png',
+  'assets/images/status/saving.png',
+  'assets/images/status/energy.png',
+];
+
 class LobbyTools extends ConsumerStatefulWidget {
   final WOTGame game;
   const LobbyTools({super.key, required this.game});
@@ -83,22 +97,73 @@ class _LobbyToolsState extends ConsumerState<LobbyTools> {
                 ),
               ],
             ),
-            Text('STATUS BAR'),
+            Column(
+              spacing: 7,
+              children: [
+                Row(
+                  spacing: 3,
+                  children: [
+                    _buildStatusIcon(
+                      icon: Image.asset(statusIcons[0]),
+                      color: const Color(0xFFFFAC9C),
+                    ),
+                    _buildStatusIcon(
+                      icon: Image.asset(statusIcons[1]),
+                      color: const Color(0xFF93D9BF),
+                    ),
+                    _buildStatusIcon(
+                      icon: Image.asset(statusIcons[2]),
+                      color: const Color(0xFFFFE77A),
+                    ),
+                  ],
+                ),
+                _buildTicketBar(),
+              ],
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildTabBar(BuildContext context) {
-    final tabBarIcons = [
-      'assets/images/tabbar_icon_home.png',
-      'assets/images/tabbar_icon_2.png',
-      'assets/images/tabbar_icon_book.png',
-      'assets/images/tabbar_icon_settings.png',
-      'assets/images/tabbar_icon_menu.png',
-    ];
+  Widget _buildStatusIcon({required Image icon, required Color color}) {
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(6),
+        border: Border.all(
+          color: const Color(0xFFAE866B),
+          width: 2,
+        ),
+      ),
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          // meter like background
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(4),
+              bottomRight: Radius.circular(4),
+            ),
+            child: Container(
+              width: 40,
+              height: 20,
+              color: color,
+            ),
+          ),
+          Container(
+            width: 40,
+            height: 40,
+            child: icon,
+          ),
+        ],
+      ),
+    );
+  }
 
+  Widget _buildTabBar(BuildContext context) {
     return Container(
       height: 62 + MediaQuery.of(context).padding.bottom,
       padding: EdgeInsets.fromLTRB(
@@ -180,6 +245,53 @@ class _LobbyToolsState extends ConsumerState<LobbyTools> {
             const SizedBox(width: 21),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildTicketBar() {
+    final ticketCount = 4;
+
+    final firstTicketRadius = BorderRadius.only(
+      topLeft: Radius.circular(3),
+      bottomLeft: Radius.circular(3),
+    );
+
+    final lastTicketRadius = BorderRadius.only(
+      topRight: Radius.circular(3),
+      bottomRight: Radius.circular(3),
+    );
+
+    return Container(
+      height: 13,
+      padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 1),
+      decoration: BoxDecoration(
+        color: const Color(0xFFFBF5F0),
+        borderRadius: BorderRadius.circular(6),
+        border: Border.all(
+          color: const Color(0xFFAE866B),
+          width: 2,
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        spacing: 1,
+        children: [
+          for (var i = 0; i < ticketCount; i++)
+            Container(
+              width: 30,
+              height: 7,
+              decoration: BoxDecoration(
+                color: const Color(0xFFF9DCA3),
+                borderRadius: i == 0
+                    ? firstTicketRadius
+                    : i == ticketCount - 1
+                        ? lastTicketRadius
+                        : BorderRadius.circular(1),
+              ),
+            ),
+        ],
       ),
     );
   }
